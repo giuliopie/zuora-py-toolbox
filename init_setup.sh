1bc6f4bc6457373db0c63770ffc1b4f374d2b135
Giulio Piepoli <giuliopiepoli.dev@gmail.com>
2023-12-01 13:14:35 +0100

    Setup project structure

--- init_setup.sh
+++ init_setup.sh
@@ -1,10 +1,10 @@
 echo "[ `date` ]": "START"
 echo "[ `date` ]": "Creating virtual env" 
-python -m venv venv/
+python3 -m venv venv/
 echo "[ `date` ]": "activate venv"
 source venv/bin/activate
 echo "[ `date` ]": "installing the requirements" 
 pip install -r requirements.txt
 echo "[ `date` ]": "creating folders and files" 
-python template.py
+python3 template.py
 echo "[ `date` ]": "END"
