45a87a318f0deb9019c74e0ba9519f551981b4fe
Giulio Piepoli <giuliopiepoli.dev@gmail.com>
2023-12-02 15:46:25 +0100

    Init db folder

---
+++ db/seeds/SBX5.py
@@ -0,0 +1,6 @@
+class SBX5:
+    def __init__(self):
+        self.label = "SBX5"
+        self.base_endpoint = os.environ.get("Z_SANDBOX_BASE_URL")
+        self.client_id = ""
+        self.client_secret = ""
---
+++ db/seeds/environment_seeder.py
@@ -0,0 +1,6 @@
+from db.seeds.SBX5 import SBX5
+
+class EnvironmentSeeder:
+    def __init__(self):
+        self.SBX5 = SBX5()
+        
