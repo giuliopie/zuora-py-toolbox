132387e7394a6adc244cb3f9621b4bfb0d760537
Giulio Piepoli <giuliopiepoli.dev@gmail.com>
2023-12-02 15:56:03 +0100

    Set db testing classes

--- db/seeds/SBX5.testing.py
+++ db/seeds/SBX5.testing.py
@@ -2,5 +2,5 @@ class SBX5:
     def __init__(self):
         self.label = "SBX5"
         self.base_endpoint = os.environ.get("Z_SANDBOX_BASE_URL")
-        self.client_id = ""
-        self.client_secret = ""
+        self.client_id = ''
+        self.client_secret = ''
