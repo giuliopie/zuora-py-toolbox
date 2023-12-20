9635cf5e0386771d82b4823eac24c2582161c6ea
Giulio Piepoli <giuliopiepoli.dev@gmail.com>
2023-12-20 23:15:48 +0100

    Setup Token class

---
+++ src/services/token.py
@@ -0,0 +1,29 @@
+import os
+import requests
+
+class Token:
+    
+    RESOURCE_PATH = 'oauth/token'
+
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    def set_bearer_token(self):
+        api_url = os.environ.get("Z_SANDBOX_BASE_URL") + self.RESOURCE_PATH
+        data = {
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "grant_type": "client_credentials"
+        }
+        formResponse = requests.post(
+            api_url,
+            data=data,
+            headers= {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        )
+        return formResponse.json()
+
+    def get_access_token(self):
+        return self.set_bearer_token()
